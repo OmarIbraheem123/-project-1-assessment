6d77ea306565562b99551f5fcbaf5b720cdcc168
Omar Ibraheem <ibraheemomar84@yahoo.com>
2023-12-12 20:08:15 -0500

    assesment

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const plus = document.getElementById('plus')
+const minus = document.getElementById('minus')
+const numBox = document.getElementById('numbox')
+plus.addEventListener('click', addNum)
+minus.addEventListener('click', minusNum)
+
+function addNum () {
+if (addNum.onclick) {
+    addNum++
+}
+}
+
+function minusNum () {
+
+}
